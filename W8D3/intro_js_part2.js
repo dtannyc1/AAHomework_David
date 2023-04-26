b20e28cc8f8c04bda6a0d3e6b3412c36cbb3c23e
dtannyc1 <dtannyc1@gmail.com>
2023-04-25 21:56:24 -0400

    W8D3 - intro js pt 2 phase 2 done

--- W8D3/intro_js_part2.js
+++ W8D3/intro_js_part2.js
@@ -7,3 +7,27 @@ const titleize = function(arr, callback) {
 const printCallback = function(arr) {
     arr.forEach(el => console.log(el));
 };
+
+//------------------------------------------------------------------------------
+const Elephant = function (name, height, tricks) {
+    this.name = name;
+    this.height = height;
+    this.tricks = tricks;
+};
+
+Elephant.prototype.trumpet = function () {
+    console.log(`${this.name} the elephant goes 'phrRRRRRRRRRRRR!!!!!!!!!!!!'`)
+}
+
+Elephant.prototype.grow = function () {
+    this.height += 12;
+}
+
+Elephant.prototype.addTrick = function (trick) {
+    this.tricks.push(trick)
+}
+
+Elephant.prototype.play = function () {
+    let num = Math.floor(Math.random() * (this.tricks.length));
+    console.log(`${this.name} is ${this.tricks[num]}!`)
+}
