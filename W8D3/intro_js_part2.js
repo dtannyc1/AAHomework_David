1632eaf1280028c95860e2e33a937a82afdbe891
dtannyc1 <dtannyc1@gmail.com>
2023-04-25 22:00:43 -0400

    W8D3 - intro js pt 2 phase 3 done

--- W8D3/intro_js_part2.js
+++ W8D3/intro_js_part2.js
@@ -31,3 +31,20 @@ Elephant.prototype.play = function () {
     let num = Math.floor(Math.random() * (this.tricks.length));
     console.log(`${this.name} is ${this.tricks[num]}!`)
 }
+
+//------------------------------------------------------------------------------
+
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.paradeHelper = function (elephant) {
+    console.log(`${elephant.name} is trotting by!`)
+}
+
+Elephant.paradeHelper(micah);
+
+herd.forEach(Elephant.paradeHelper)
