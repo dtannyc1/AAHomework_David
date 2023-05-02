925acdfc7b03f1f3ca97d430ec3b44c2db71261d
dtannyc1 <dtannyc1@gmail.com>
2023-05-01 20:53:31 -0400

    W9D2 - Finished Phase III

--- W9D2/skeleton/script.js
+++ W9D2/skeleton/script.js
@@ -31,8 +31,8 @@ document.addEventListener("DOMContentLoaded", () => {
     ul.appendChild(li);
   }
 
-  let buttons = document.getElementsByClassName("favorite-submit");
-  buttons[0].addEventListener("click", event => {
+  let sfFavSubmit = document.getElementsByClassName("favorite-submit");
+  sfFavSubmit[0].addEventListener("click", event => {
     event.preventDefault();
     submitListItem(event);
   });
@@ -41,6 +41,36 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // --- your code here!
 
+
+  const togglePhotoForm = (e) => {
+    let form = document.getElementsByClassName("photo-form-container");
+    form[0].classList.toggle('hidden')
+  }
+
+  let photoToggle = document.getElementsByClassName("photo-show-button");
+  photoToggle[0].addEventListener("click", event => {
+    event.preventDefault();
+    togglePhotoForm(event);
+  });
+
+  const submitPhotoForm = (e) => {
+    let input = document.getElementsByClassName("photo-url-input");
+    let imgLink = input[0].value;
+    input[0].value = "";
+
+    const ul = document.getElementsByClassName("dog-photos")[0];
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = imgLink;
+    li.appendChild(img);
+    ul.appendChild(li);
+  };
+
+  let photoSubmit = document.getElementsByClassName("photo-url-submit");
+  photoSubmit[0].addEventListener("click", event => {
+    event.preventDefault();
+    submitPhotoForm(event);
+  });
 
 
 });
