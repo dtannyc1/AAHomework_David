024f1654ebd2f962f629dc68f06a72332625c1e2
dtannyc1 <dtannyc1@gmail.com>
2023-05-02 21:51:53 -0400

    W9D3 - Finished. Why would the weather website use Kelvin?

--- W9D3/practice-for-ch-js-first-ajax-main/index.js
+++ W9D3/practice-for-ch-js-first-ajax-main/index.js
@@ -1,3 +1,21 @@
 console.log("Hello from the console!");
 
 // Your code here
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bcb83c4b54aee8418983c2aff3073b3b')
+    .then((response) => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error(response);
+        }
+    })
+    .then((successResponse) => {
+        console.log("Success!")
+        console.log(successResponse);
+    }, (failureResponse) => {
+        console.log("Failure!");
+        console.log(failureResponse);
+    })
+
+console.log('The AJAX request has been dispatched');
