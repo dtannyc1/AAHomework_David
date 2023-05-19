13362fda3793e2c18b1f262b0a4a2171664ab653
dtannyc1 <dtannyc1@gmail.com>
2023-05-18 23:32:51 -0400

    W11D5 - finished thunk action creator

--- W11D5/practice-for-ch-react-thunk-action-creator-main/frontend/src/store/articleReducer.js
+++ W11D5/practice-for-ch-react-thunk-action-creator-main/frontend/src/store/articleReducer.js
@@ -1,9 +1,29 @@
-import articles from '../data/data.json';
+// import articles from '../data/data.json';
 
 const LOAD_ARTICLES = 'article/loadArticles';
 const ADD_ARTICLE = 'article/addArticle';
 
-export const loadArticles = () => {
+export const fetchArticles = () => async (dispatch) => {
+    const response = await fetch('/api/articles');
+    const articles = await response.json();
+    dispatch(loadArticles(articles));
+}
+
+export const writeArticle = (payload) => async (dispatch) => {
+    const response = await fetch('/api/articles', {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(payload)
+    });
+    if (response.ok){
+        const article = await response.json();
+        dispatch(addArticle(article));
+    }
+}
+
+export const loadArticles = (articles) => {
   return {
     type: LOAD_ARTICLES,
     articles
