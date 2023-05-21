cbdc790c934ce63eb2f0d9943ff850b27ff2a66b
dtannyc1 <dtannyc1@gmail.com>
2023-05-21 13:26:15 -0400

    W12D1 - finished routes

--- W12D1/practice-for-ch-react-jbuilder-long-practice-main/config/routes.rb
+++ W12D1/practice-for-ch-react-jbuilder-long-practice-main/config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    # Defines the root path route ("/")
+    # root "articles#index"
+
+    namespace :api, defaults: {format: :json} do
+        resources(:guests, only: [:index, :show]) {
+            resources(:gifts, only: [:index])
+        }
+
+        resources(:gifts, only: [:show])
+
+        resources(:parties, only: [:index, :show])
+    end
 end
